62271014308c92be32fa179f314e21f930e68f52
wattyla <wattylaa@hotmail.com>
2021-01-01 18:13:56 -0300

    Primeiro Commit realizado no projeto.

---
+++ PrimeiroProjetoJDBC/src/br/wattyla/jdbc1/aplicacao/Programa.java
@@ -0,0 +1,24 @@
+package br.wattyla.jdbc1.aplicacao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import br.wattyla.jdbc1.db.DB;
+import br.wattyla.jdbc1.db.InsercaoDados;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		//Aula de recupera dos dados do Banco (consulta).
+		//new RecuperacaoDados().recuperaDadosBanco();
+		//Aula de insercão de dados no Banco (insert).
+		//new InsercaoDados().insercaoDados();
+
+	}
+
+}
